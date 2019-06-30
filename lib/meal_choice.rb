041852cdabf4715a0b9ffbb850ffb83909d60bef
Austin Trow <austintrow@gmail.com>
2019-06-30 17:37:40 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,5 @@
 def meal_choice(veg1, veg2, protein)
   puts "What a nutrious meal!"
-  puts "A plate "
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  return "A plate of #{protein} with #{veg1} and #{veg2}."
 end
